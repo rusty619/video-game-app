05861547a1d199713605719596d48379d737b0be
Ruslan Khait <rusty6916@gmail.com>
2022-05-11 21:58:53 -0400

    sold cloumn default to false again

--- video-game-app-server/db/migrate/20220512003801_add_sold_to_video_games.rb
+++ video-game-app-server/db/migrate/20220512003801_add_sold_to_video_games.rb
@@ -1,5 +1,5 @@
 class AddSoldToVideoGames < ActiveRecord::Migration[6.1]
   def change
-    add_column :video_games, :sold , :boolean
+    add_column :video_games, :sold , :boolean, :default => false
   end
 end
