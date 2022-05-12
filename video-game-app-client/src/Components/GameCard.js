cf911e81103cee4f0aa0da8288318732fd843904
Ruslan Khait <rusty6916@gmail.com>
2022-05-11 20:02:08 -0400

    update improves with css

--- video-game-app-client/src/Components/GameCard.js
+++ video-game-app-client/src/Components/GameCard.js
@@ -13,19 +13,23 @@ function GameCard({videoGame}) {
             <div className="video-game-item">
                 <img src={img}/>
                 <div className="desc">
-                    {title}
-                    <br/>
-                    Condition: {condition}
-                    <br/>
-                    Genre: {genre}
-                    <br/>
-                    Developer: {developer}
-                    <br/>
-                    Review: <span className="star-rating">{showStarRating()}</span>
-                    <br/>
-                    Platform: {name}
-                    <br/>
-                    Price: ${value}
+                    <h3>{title}</h3>
+                    
+                    <h3>Condition: {condition}</h3>
+                    
+                    <h3>Genre: {genre}</h3>
+                    
+                    <h3>Developer: {developer}</h3>
+                    
+                    <h3>Review: <span className="star-rating">{showStarRating()}</span></h3>
+                    
+                    <h3>Platform: {name}</h3>
+                    
+                    <h3>Price: ${value}</h3>
+                    
+                    <div className="btn-section">
+                        <button className="buy-btn">buy</button>
+                    </div>
                 </div>
             </div>
         )
